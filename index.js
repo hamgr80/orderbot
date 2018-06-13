022d08f28e76f60cc7e9f5612f2110eda0d8fc29
hamgr80 <38524373+hamgr80@users.noreply.github.com>
2018-06-13 15:15:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post('/webhook', function (req, res) {
 	                          "Password":"pwd123",
 	                          "ReturnType":"json"
                           });
-    console.log('response: ' + JSON.stringify(res));
+    console.log('response: ' + res.getBody('utf8'));
     console.log('response2: ' + JSON.parse(res.getBody('utf8')));
     console.log('user id: ' + lineUserId + ' authenticated = ' + JSON.parse(res).Success);
     
