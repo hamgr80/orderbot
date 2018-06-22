546df74eaec2af8ee202f181a472d27268604b59
hamgr80 <38524373+hamgr80@users.noreply.github.com>
2018-06-22 18:26:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const syncRequest = require('sync-request')
-
+const bodyParser = require('body-parser')
 
 const app = express()
-
+app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
 
 const REQUIRE_AUTH = true
@@ -73,4 +73,8 @@ app.post('/webhook', function (req, res) {
   }
   
   
+})
+
+app.listen(app.get('port'), function () {
+  console.log('* Webhook service is listening on port:' + app.get('port'))
 })
