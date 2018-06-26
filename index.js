6a783b2c0adb5c505d15481fdafbe2f4e7b131ae
hamgr80 <38524373+hamgr80@users.noreply.github.com>
2018-06-26 01:47:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook', function (req, res) {
               "ReturnType":"json",
               "IntentKey":INTENT_NAME}
       });
-    
+    console.log(JSON.parse(JSON.parse(resSR.getBody('utf8'))).Message);
     webhookReply = JSON.parse(JSON.parse(JSON.parse(resSR.getBody('utf8'))).Message)[0].VALUE;
     res.status(200).json({
         	source: 'webhook',
